b6446b68eb129bb4b1418ad7411b39ad3329e142
Jon Rexeisen <jrexeise@nerdery.com>
2013-07-31 14:45:50 -0500

    * Updated AFNetworking to latest version * Updated application assets * Updated documentation and avoided compiler warnings in some cases

--- Umbrella/ThirdParty/WeatherUndergroundAPI/WeatherAPIClient.h
+++ Umbrella/ThirdParty/WeatherUndergroundAPI/WeatherAPIClient.h
@@ -6,12 +6,10 @@
 //  Copyright (c) 2012 The Nerdery. All rights reserved.
 //
 
-#import <CoreLocation/CoreLocation.h>
 #import "AFNetworking.h"
 
-/*!
- @typedef WeatherAPICompletionBlock
- @abstract A block that will be called when the API request is complete.
+/**
+ A block that will be called when the API request is complete.
  @param success YES if the request was successful
  @param error An NSError that occurred, if there was one (will only be present if the request was not successful)
  */
@@ -22,34 +20,30 @@ extern NSString * const kWeatherAPIClientErrorDomain;
 extern NSInteger const kWeatherAPIClientErrorCodeGeneric;
 extern NSInteger const kWeatherAPIClientErrorCodeNoAPIKey;
 
+/**
+ This class will call out to the Weather Client and fetch the appropriate data
+ */
 @interface WeatherAPIClient : AFHTTPClient
 
+/**
+ Required for making calls to the weather underground API.
+ */
 @property (nonatomic, strong) NSString *APIKey;
 
 #pragma mark - Singleton
 
-/*!
- @method sharedClient
- @abstract Singleton for the WUnderground API.
+/**
+ Singleton for the WUnderground API.
  */
 + (WeatherAPIClient *)sharedClient;
 
 #pragma mark - Fetchers
 
-/*!
- @method getForcastAndConditionsForZipCode:withCompletionBlock:
- @abstract Gets the gets the forcast and conditions from the Weather Underground API and calls the completion block on success/failure. Must be called on main thread.
+/**
+ Gets the gets the forcast and conditions from the Weather Underground API and calls the completion block on success/failure. Must be called on main thread.
  @param zipCode the 5 digit zip code of the location to retrieve the weather
  @param completionBlock The completion block to be executed when the request has completed. The completion block will be called on the main thread.
  */
 - (void)getForcastAndConditionsForZipCode:(NSString *)zipCode withCompletionBlock:(WeatherAPICompletionBlock)completionBlock;
-
-/*!
- @method getForcastAndConditionsForZipCode:withCompletionBlock:
- @abstract Gets the forcast and conditions from the Weather Underground API and calls the completion block on success/failure. Must be called on main thread. Does nothing but return NO if there is already a request in progress. The completion block is not guaranteed to be called on the main thread.
- @param location The Core Location object of the location to retrieve the weather
- @param completionBlock The completion block to be executed when the request has completed. The completion block will be called on the main thread.
- */
-- (void)getForcastAndConditionsForLocation:(CLLocation *)location withCompletionBlock:(WeatherAPICompletionBlock)completionBlock;
 
 @end
